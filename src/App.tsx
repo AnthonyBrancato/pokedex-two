bfe92f33bc35238a0bcbcffc7948db901696967b
Anthony Brancato <klIpTs2016#2>
2022-01-15 17:43:19 +0100

    fix(App): remove console log

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import "./App.css";
 function App() {
   const { pokemonsType } = usePokemonsTypeQuery();
 
-  console.log(pokemonsType);
-
   return (
     <div className="App">
       <p>Nb of type: {pokemonsType?.count}</p>
